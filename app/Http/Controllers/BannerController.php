b4c4e8cd133f0a4fb404f19588572d31760d7bdf
Marcelo Travasso <marceloctj@gmail.com>
2021-01-15 20:15:54 -0300

    Adiciona modificacoes da ultima aula ao vivo

---
+++ app/Http/Controllers/BannerController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+use App\Models\Banner;
+use App\Models\HomeSection;
+
+use Validator;
+use Carbon\Carbon;
+
+class BannerController extends Controller
+{
+    public function create(Request $request)
+    {
+        $form = optional([
+            'home_section_id' => $request->old('home_section_id', $request->get('section_id', null)),
+        ]);
+
+        if ($request->has('section_id')) {
+            session()->flash('section_active_id', $request->get('section_id'));
+        }
+
+        $sections = HomeSection::where('type', 'banners')->get();
+
+        return view('banners.create', compact('sections', 'form'));
+    }
+
+    public function store(Request $request)
+    {
+        $section = HomeSection::find($request->get('home_section_id'));
+
+        $rules = [
+            'home_section_id' => 'required|exists:home_sections,id',
+            'available_at' => 'required|date_format:d/m/Y',
+            'expires_in' => $request->get('expires_in') ? 'date_format:d/m/Y': '',
+            'image'      => [
+                'required',
+                'image',
+                $section ? Rule::dimensions()
+                    ->ratio(
+                        $section->dimensions ? $section->dimensions['width'] / $section->dimensions['height'] : '1/1'
+                    )
+                    ->maxWidth($section->dimensions['width'] * 2)
+                    ->maxHeight($section->dimensions['height'] * 2) : null,
+            ]
+        ];
+
+        $request->validate($rules);
+
+        $data = $request->all();
+        $data['image'] = asset("storage/" . $request->file('image')->store('images/banners', 'public'));
+
+        $data['available_at'] = Carbon::createFromFormat('d/m/Y', $data['available_at'])->startOfDay();
+
+        if ($data['expires_in'])
+            $data['expires_in'] = Carbon::createFromFormat('d/m/Y', $data['expires_in'])->endOfDay();
+
+        $section->banners()->create($data);
+
+        cache()->forget('dashboard-data');
+
+        return redirect()->route('home_section.index')->with(['section_active_id' => $section->id]);
+    }
+
+    public function destroy($id)
+    {
+        $banner = Banner::find($id);
+
+        if (!$banner)
+            return redirect()->back()->withErrors(['message' => 'Banner não existe']);
+
+        $banner->delete();
+
+        cache()->forget('dashboard-data');
+
+        return redirect()->route('home_section.index')->with(['section_active_id', $banner->home_section_id]);
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 require('./bootstrap');
+
+try {
+    const datepicker = require('js-datepicker');
+    $('.datepicker').each((_, el) => {
+        datepicker(el, {
+            customDays: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+            customMonths: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            formatter: (input, date) => {
+                const value = date.toLocaleDateString()
+                input.value = value;
+            },
+        });
+    });
+
+    $('[data-confirm]').on('submit', function(e) {
+        e.preventDefault();
+        if(confirm($(this).data('confirm'))) {
+            this.submit();
+        }
+    });
+
+    $('[data-search-table]').on('keyup', function() {
+        const value = $(this).val().toLowerCase();
+        const target = $(this).data('search-table');
+
+        $(`${target} tr`).filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+} catch (err) {}
